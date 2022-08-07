d92fa364e8251c3685d3fbcbba49fe870d7ac673
Linh Vu <linh777gkog@gmail.com>
2022-08-08 02:32:05 +0700

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
-import { Navbar } from './Navbar';
 
 import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
